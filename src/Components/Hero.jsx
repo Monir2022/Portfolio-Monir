40fd07dfd95816cf3bec6c4c31b9e1832ccdffe2
Monir Akhand <akhandbuet@gmail.com>
2022-02-26 19:38:51 +0100

    hero is done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
-function Hero(props) {
+import "../Styles/hero.css";
+export default function Hero( ) {
     return (
-        <div>
-            <h1>I am a Hero</h1>
+        <div className='hero'>
+            <h1 className='hero-heading'>Hi, I am Monir Akhand a Frontend developer from Sweden</h1>
         </div>
     );
 }
 
-export default Hero;
