b06b666e0aa61bd482dd0fe3cc35b625432b5377
Monir Akhand <akhandbuet@gmail.com>
2022-03-02 16:08:54 +0100

    Technologies section updated

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,35 +1,59 @@
+////Project files
 import "../Styles/card.css";
 import Modal from "./Modal";
 
-  
+//NPM Packages
+
 import { useState } from "react";
 
-export default function Card({firstImg, pname, discription, repo, demo}) {
+export default function Card({ firstImg, pname, discription, repo, demo }) {
   const [modal, setModal] = useState(false);
 
-  const Toggle = () => setModal(!modal);
+  const ToggleModal = () => setModal(!modal);
 
-    return (
-      <div className="container">
-      <img src={firstImg} alt="Avatar" className="image"/>
+  return (
+    <div className="container">
+      <img src={firstImg} alt="Avatar" className="image" />
       <div className="overlay">
         <div className="text">
           <p>{pname}</p>
-          <button onClick={() => Toggle()}>More</button>
-          <Modal show={modal} close={Toggle} title={pname}>
+          <button onClick={() => ToggleModal()}>More</button>
+          <Modal show={modal} close={ToggleModal} title={pname}>
             <div className="modal-child">
-              <img src={firstImg} alt="pict"  className="modal-img"/>
+              <img src={firstImg} alt="pict" className="modal-img" />
               <div className="discriptions">
-                  <p>{discription}</p>
-                   {demo?<a className="demo_src" href={demo} target="_blank" rel="noopener noreferrer"> Live Demo</a>: <h6>Demo Not Available</h6>}
-                   {repo?<a className="demo_src" href={repo} target="_blank" rel="noopener noreferrer"> View Source</a>: <h6>Comming Soon</h6>}
-                   
+                <p>{discription}</p>
+                {demo ? (
+                  <a
+                    className="demo_src"
+                    href={demo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {" "}
+                    Live Demo
+                  </a>
+                ) : (
+                  <h6>Demo Not Available</h6>
+                )}
+                {repo ? (
+                  <a
+                    className="demo_src"
+                    href={repo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {" "}
+                    View Source
+                  </a>
+                ) : (
+                  <h6>Comming Soon</h6>
+                )}
               </div>
-           </div>
+            </div>
           </Modal>
         </div>
       </div>
     </div>
-    );
+  );
 }
-
