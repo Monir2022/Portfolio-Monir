ec69459a919e92e7eacf804e31efa4f61356d548
Monir Akhand <akhandbuet@gmail.com>
2022-03-17 13:40:28 +0100

    Technology

--- src/Components/Tech.jsx
+++ src/Components/Tech.jsx
@@ -2,7 +2,6 @@
 import Techstack from "./Techstack";
 import techData from "../Data/technologyList.json";
 
-
 export default function Tech() {
   const technologyData = techData.map((item) => (
     <Techstack key={item.id} techName={item.title} techIcon={item.logo} />
@@ -13,10 +12,6 @@ export default function Tech() {
       <h2 className="tech-title">Technologies</h2>
       <div className="tech-container">
         <p>
-          Here is the techology that I already know and also starting to learn.
-          Here is the techology that I already know and also starting to learn.
-          Here is the techology that I already know and also starting to learn.
-          Here is the techology that I already know and also starting to learn.
           Here is the techology that I already know and also starting to learn.
         </p>
         <div className="tech-list">{technologyData}</div>
